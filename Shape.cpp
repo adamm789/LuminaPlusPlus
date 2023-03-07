1a2133fbc1014f7d9ff9f4efaedd4f0f4772e1fd
adamm789 <musician903@gmail.com>
2023-03-07 17:05:31 -0600

    shape stuff

--- Shape.cpp
+++ Shape.cpp
@@ -1,5 +1,7 @@
 #include "Shape.h"
 
-Shape::~Shape() {
-
+Shape::Shape(std::string shapeName, uint16_t startIndex[3], uint16_t meshCount[3]) {
+	ShapeName = shapeName;
+	ShapeMeshStartIndex = startIndex;
+	ShapeMeshCount = meshCount;
 }
--- Shape.h
+++ Shape.h
@@ -1,10 +1,22 @@
 #pragma once
-
+#include "MdlStructs.h"
 #include <string>
+#include <vector>
+#include <map>
+#include "Vertex.h"
 class Shape
 {
+	// https://github.com/xivdev/Penumbra/blob/master/Penumbra.GameData/Files/MdlFile.cs
 public:
-	~Shape();
 	std::string ShapeName;
+	uint16_t* ShapeMeshStartIndex;
+	uint16_t* ShapeMeshCount;
+
+	std::vector<MdlStructs::ShapeValueStruct> ShapeValueStructs;
+
+	uint16_t ShapeValuesStartIndex = 0;
+
+
+	Shape(std::string shapeName, uint16_t startIndex[3], uint16_t meshCount[3]);
 };
 
