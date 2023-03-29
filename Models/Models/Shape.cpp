79b3e0defbac93727815039e5e8ec695ed622e2b
adamm789 <musician903@gmail.com>
2023-03-29 15:46:05 -0500

    Shape cleanup

--- Models/Models/Shape.cpp
+++ Models/Models/Shape.cpp
@@ -2,6 +2,8 @@
 
 Shape::Shape(std::string shapeName, uint16_t startIndex[3], uint16_t meshCount[3]) {
 	ShapeName = shapeName;
-	ShapeMeshStartIndex = startIndex;
-	ShapeMeshCount = meshCount;
+	//ShapeMeshStartIndex = *startIndex;
+	memcpy(&ShapeMeshStartIndex, &startIndex, 3);
+	//ShapeMeshCount = meshCount;
+	memcpy(&ShapeMeshCount, &meshCount, 3);
 }
--- Models/Models/Shape.h
+++ Models/Models/Shape.h
@@ -9,13 +9,12 @@ class Shape
 	// https://github.com/xivdev/Penumbra/blob/master/Penumbra.GameData/Files/MdlFile.cs
 public:
 	std::string ShapeName;
-	uint16_t* ShapeMeshStartIndex;
-	uint16_t* ShapeMeshCount;
+	uint16_t ShapeMeshStartIndex[3];
+	uint16_t ShapeMeshCount[3];
 
 	std::vector<MdlStructs::ShapeValueStruct> ShapeValueStructs;
 
 	uint16_t ShapeValuesStartIndex = 0;
-
 
 	Shape(std::string shapeName, uint16_t startIndex[3], uint16_t meshCount[3]);
 };
